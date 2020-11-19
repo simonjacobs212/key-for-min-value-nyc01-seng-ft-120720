9b23fce3e601c0162b0fd827f8f30cf8e5b8d3c6
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-19 16:44:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+
 
 def key_for_min_value(name_hash)
-
+  name_hash.map do |e|
+    e.min()
+  end
+   
 end
+
+# prereqs: iterators, hashes, conditional logic
+# Given a hash with numeric values, return the key for the smallest value
